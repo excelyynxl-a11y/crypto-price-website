dccd7352b842fe3823dc00fefa55224fb30619d6
excel <elim0079@student.monash.edu>
2025-11-20 09:20:11 +0800

    create skeleton for Home and CoinDetail

---
+++ src/pages/CoinDetail.jsx
@@ -0,0 +1,7 @@
+export const CoinDetail = () => {
+    return (
+        <div>
+            This is CoinDetail.
+        </div>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+    return (
+        <div>
+            This a HomePage.
+        </div>
+    );
+}
